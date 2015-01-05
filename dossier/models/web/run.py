6f2731591e76d82874e328d0e57b98caf7fcddcf
Andrew Gallant <andrew@diffeo.com>
2015-01-05 15:01:39 -0500

    Add tfidf support to creating an FC from HTML.
    This also removes support for materializing an FC when running a
    search using a non-existent query. I claim that this shouldn't happen
    in correct code. Moreover, it can never be right because the backend
    cannot scrape pages reliably. The HTML *must* be sent by the user
    explicitly.
    
    Start working on a test for it too...

--- dossier/models/web/run.py
+++ dossier/models/web/run.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import json
 import logging
 import os.path as path
 import urllib
@@ -10,10 +9,13 @@ import bottle
 from gensim import models
 import requests
 
-from dossier.fc import FeatureCollection, StringCounter
+from dossier.fc import StringCounter
 from dossier.models import etl
 from dossier.models.pairwise import dissimilar, similar
 import dossier.web as web
+import dossier.web.config as config
+import dossier.web.routes as routes
+import yakonfig
 
 
 logger = logging.getLogger(__name__)
@@ -35,7 +37,7 @@ def add_routes(app):
         return bottle.static_file(name, root=web_static_path)
 
     @app.put('/dossier/v1/feature-collection/<cid>')
-    def v1_fc_put(request, response, store, cid):
+    def v1_fc_put(request, response, store, tfidf, cid):
         '''Store a single feature collection.
 
         The route for this endpoint is:
@@ -45,59 +47,23 @@ def add_routes(app):
         collection. The feature collection should be in the request
         body serialized as JSON.
 
+        Alternatively, if the request's ``Content-type`` is
+        ``text/html``, then a feature collection is generated from the
+        HTML.
+
         This endpoint returns status ``201`` upon successful storage.
         An existing feature collection with id ``content_id`` is
         overwritten.
         '''
         if request.headers.get('content-type', '').startswith('text/html'):
             url = urllib.unquote(cid.split('|', 1)[1])
-            fc = create_fc_from_html(url, request.body.getvalue())
+            fc = create_fc_from_html(url, request.body.getvalue(),
+                                     tfidf=tfidf)
+            logger.info('created FC for "%r": %r', cid, fc)
+            store.put([(cid, fc)])
+            response.status = 201
         else:
-            fc = FeatureCollection.from_dict(json.load(request.body))
-        store.put([(cid, fc)])
-        response.status = 201
-
-
-class index_scan_make_fc(web.engine_index_scan):
-    '''Index scan with FC materialization.
-
-    This search engine knows the format of a particular breed of content
-    id, which enables this search engine to create FCs for that id.
-
-    Namely, if a content_id starts with ``web|``, then everything following
-    that prefix is a URL. We try to do a very basic and minimal crawl of
-    that URL to produce an FC to query with.
-    '''
-    def __init__(self, store, tfidf_path=None):
-        self.store = store
-        self._tfidf_path = tfidf_path
-        self._tfidf = None
-
-    @property
-    def tfidf(self):
-        # This lazily loads the TF-IDF model. This is done *every* time a new
-        # FC is made. This is probably OK because FCs are not made this way
-        # in bulk, only when the user specifically requests to search with
-        # a query that doesn't have an FC.
-        #
-        # On the other hand, if the model is sufficiently large, that could
-        # negatively affect latency. I'm calling that a premature optimization
-        # for now. ---AG
-        if self._tfidf is None and self._tfidf_path is not None:
-            self._tfidf = models.TfidfModel.load(self._tfidf_path)
-        return self._tfidf
-
-    def get_query_fc(self, content_id):
-        fc = super(index_scan_make_fc, self).get_query_fc(content_id)
-        if fc is not None:
-            return fc
-        if not content_id.startswith('web|'):
-            return None
-        url = urllib.unquote(content_id.split('|', 1)[1])
-        html = requests.get(url).text
-        fc = create_fc_from_html(url, html, tfidf=self.tfidf)
-        self.store.put([(content_id, fc)])
-        return fc
+            return routes.v1_fc_put(request, response, store, cid)
 
 
 def create_fc_from_html(url, html, tfidf=None):
@@ -119,10 +85,19 @@ def get_application():
         'dissimilar': dissimilar,
         'similar': similar,
         'random': web.engine_random,
-        'index_scan': index_scan_make_fc,
+        'index_scan': web.engine_index_scan,
     }
     args, application = web.get_application(routes=[add_routes],
                                             search_engines=engines)
+
+    tfidf_model = None
+    try:
+        tfidf_path = yakonfig.get_global_config('dossier.models')['tfidf_path']
+        tfidf_model = models.TfidfModel.load(tfidf_path)
+    except KeyError:
+        pass
+    application.install(
+        config.create_injector('tfidf', lambda: tfidf_model))
     return args, application
 
 
