62d66317c920c3ee4ca2b7202f0fbffac0d293e2
Andrew Gallant <andrew@diffeo.com>
2015-03-24 11:04:12 -0400

    Small refactoring to report generation.
    The biggest change is that the `Folders` subclass was removed.
    The issue is that an instance of the subclass is hard to create
    inside of web routes due to how our dependency injection works.
    Hopefully this limitation will get fixed soon...

--- dossier/models/subtopic.py
+++ dossier/models/subtopic.py
@@ -7,45 +7,44 @@ from dossier.fc import StringCounter
 import dossier.web as web
 
 
-class Folders(web.Folders):
-    def subtopics(self, folder_id, subfolder_id, ann_id=None):
-        '''Yields an unordered generator of subtopics in a subfolder.
+def subtopics(folders, folder_id, subfolder_id, ann_id=None):
+    '''Yields an unordered generator of subtopics in a subfolder.
 
-        Each item of the generator is a 4-tuple of ``content_id``,
-        ``subtopic_id``, ``subtopic_type`` and ``data``. Subtopic type
-        is one of the following Unicode strings: ``text``, ``image``
-        or ``manual``. The type of ``data`` is dependent on the
-        subtopic type. For ``image``, ``data`` is a ``(unicode, str)``,
-        where the first element is the URL and the second element is
-        the binary image data. For all other types, ``data`` is a
-        ``unicode`` string.
+    Each item of the generator is a 4-tuple of ``content_id``,
+    ``subtopic_id``, ``subtopic_type`` and ``data``. Subtopic type
+    is one of the following Unicode strings: ``text``, ``image``
+    or ``manual``. The type of ``data`` is dependent on the
+    subtopic type. For ``image``, ``data`` is a ``(unicode, str)``,
+    where the first element is the URL and the second element is
+    the binary image data. For all other types, ``data`` is a
+    ``unicode`` string.
 
-        :param str folder_id: Folder id
-        :param str subfolder_id: Subfolder id
-        :param str ann_id: Username
-        :rtype: generator of
-                ``(content_id, subtopic_id, subtopic_type, data)``
-        '''
-        # This code will be changed soon. In essence, it implements the
-        # convention established in SortingDesk for storing subtopic data.
-        # Currently, subtopic data is stored in the FC that the data (i.e.,
-        # image or snippet) came from. This is bad because it causes pretty
-        # severe race conditions.
-        #
-        # Our current plan is to put each subtopic datum in its own FC. It will
-        # require this code to make more FC fetches, but we should be able to
-        # do it with one `store.get_many` call.
-        items = self.grouped_items(folder_id, subfolder_id, ann_id=ann_id)
-        fcs = {cid: fc for cid, fc in self.store.get_many(items.keys())}
-        for cid, subids in items.iteritems():
-            fc = fcs[cid]
-            for subid in subids:
-                try:
-                    data = typed_subtopic_data(fc, subid)
-                except KeyError:
-                    # We have a dangling label folks!
-                    continue
-                yield cid, fc['meta_url'], subid, subtopic_type(subid), data
+    :param str folder_id: Folder id
+    :param str subfolder_id: Subfolder id
+    :param str ann_id: Username
+    :rtype: generator of
+            ``(content_id, subtopic_id, subtopic_type, data)``
+    '''
+    # This code will be changed soon. In essence, it implements the
+    # convention established in SortingDesk for storing subtopic data.
+    # Currently, subtopic data is stored in the FC that the data (i.e.,
+    # image or snippet) came from. This is bad because it causes pretty
+    # severe race conditions.
+    #
+    # Our current plan is to put each subtopic datum in its own FC. It will
+    # require this code to make more FC fetches, but we should be able to
+    # do it with one `store.get_many` call.
+    items = folders.grouped_items(folder_id, subfolder_id, ann_id=ann_id)
+    fcs = {cid: fc for cid, fc in folders.store.get_many(items.keys())}
+    for cid, subids in items.iteritems():
+        fc = fcs[cid]
+        for subid in subids:
+            try:
+                data = typed_subtopic_data(fc, subid)
+            except KeyError:
+                # We have a dangling label folks!
+                continue
+            yield cid, fc['meta_url'], subid, subtopic_type(subid), data
 
 
 def typed_subtopic_data(fc, subid):
